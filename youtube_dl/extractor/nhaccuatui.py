24acb3eb721a74771b96adac7ddf5fd477007a37
Võ Văn Khoa <khoazero123@gmail.com>
2017-10-17 10:02:17 +0700

    Update nhaccuatui

--- youtube_dl/extractor/nhaccuatui.py
+++ youtube_dl/extractor/nhaccuatui.py
@@ -5,10 +5,15 @@ from .common import InfoExtractor
 from ..compat import compat_xpath
 from ..utils import (
     xpath_text,
+    int_or_none,
+    str_or_none,
 )
-
+"""
+python -m youtube_dl http://www.nhaccuatui.com/playlist/bai-hat-yeu-thich-khoazero123-dang-cap-nhat.wEBAgXwNPtMW.html -F --cookies "cookies/nhaccuatui.txt.txt"
+python -m youtube_dl http://www.nhaccuatui.com/playlist/bai-hat-yeu-thich-khoazero123-dang-cap-nhat.wEBAgXwNPtMW.html -o "download/%(title)s - %(id)s.%(ext)s" -F --cookies "cookies/nhaccuatui.txt.txt"
+"""
 class NhacCuaTuiIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?nhaccuatui\.com/(?:bai-hat|album|playlist|video)/[^\.]+\.(?P<id>\w+)\.html'
+    _VALID_URL = r'https?://(?:www\.)?nhaccuatui\.com/(?:bai-hat|album|playlist|video|user)/[^\.]+\.(?P<id>\w+)\.html'
     _TESTS = [{
         'url': 'http://www.nhaccuatui.com/bai-hat/yeu-di-dung-so-yeu-di-dung-so-ost-onlyc.J8kPKRvgBXEK.html',
         'info_dict': {
@@ -18,6 +23,21 @@ class NhacCuaTuiIE(InfoExtractor):
             'description': 'Bài hát: Yêu Đi Đừng Sợ (Yêu Đi Đừng Sợ OST) - OnlyC..Có một quá khứ phải cố quên mỗi khi kề bên.Và có một nỗi nhớ dần lớn lê',
             'thumbnail': r're:^https?://.*\.jpg$',
         }
+    }, {
+        'url': 'http://www.nhaccuatui.com/playlist/bai-hat-yeu-thich-khoazero123-dang-cap-nhat.wEBAgXwNPtMW.html',
+        'info_dict': {
+            'id': 'wEBAgXwNPtMW',
+            'ext': 'mp3',
+            'title': 'Album Bài hát yêu thích khoazero123',
+        },
+    }, {
+        'url': 'http://www.nhaccuatui.com/user/khoazero123.nghe-nhac-cua-tui.html',
+        'info_dict': {
+            'id': 'nghe-nhac-cua-tui',
+            'ext': 'mp3',
+            'title': 'Album Bài hát yêu thích khoazero123',
+        },
+        'only_matching': True,
     }, {
         'url': 'http://www.nhaccuatui.com/video/giot-nuoc-mat-trong-mo-trung-quang.3Wbm11Y4529m5.html',
         'info_dict': {
@@ -29,10 +49,84 @@ class NhacCuaTuiIE(InfoExtractor):
         },
         'only_matching': True,
     }]
+    def _extract_item(self, item, page_type, fatal=True):
+        item = item
+        formats = []
+
+        title_item = item.find('title').text.strip()
+        id_item = item.find('key').text.strip()
+        creator_item = item.find('creator').text.strip()
+        location_item = item.find('location').text.strip()
+        kbit = int_or_none(item.find('kbit').text.strip())
+        locationHQ_item = item.find('locationHQ').text.strip() # 320
+        lossless_item = str_or_none(item.find('hasHQ').text.strip()) # lossless
+
+        quality_item = 128
+        #if kbit and not locationHQ_item:
+            #pass
+            #quality_item = int_or_none(kbit)
+
+        f = {
+            'format_id': str_or_none(quality_item),
+            'url': location_item,
+        }
+        if page_type == 'video':
+            f.update({
+                'height': quality_item,
+                'ext': 'mp4',
+            })
+        else:
+            f.update({
+                'abr': quality_item,
+                'ext': 'mp3',
+            })
+        formats.append(f)
+
+        if locationHQ_item:
+            f = {
+                'format_id': str_or_none(kbit),
+                'url': locationHQ_item,
+            }
+            if page_type == 'video':
+                f.update({
+                    'height': quality_item,
+                    'ext': 'mp4',
+                })
+            else:
+                f.update({
+                    'abr': quality_item,
+                    'ext': 'mp3',
+                })
+            formats.append(f)
+        if lossless_item == 'true':
+            lossless_json = self._download_json('http://www.nhaccuatui.com/download/song/'+id_item+'_lossless', id_item, 'Downloading lossless JSON')
+            if 'stream_url' in lossless_json['data']:
+                f = {
+                    'format_id': 'lossless',
+                    'url': lossless_json['data']['stream_url'],
+                }
+                if page_type == 'video':
+                    f.update({
+                        'height': quality_item,
+                        'ext': 'mp4',
+                    })
+                else:
+                    f.update({
+                        'abr': quality_item,
+                        'ext': 'mp3',
+                    })
+                formats.append(f)
+
+        return {
+            'title': title_item,
+            'id': id_item,
+            'formats': formats,
+        }
+
 
     def _real_extract(self, url):
-        video_id = self._match_id(url)
-        webpage = self._download_webpage(url, video_id)
+        id = self._match_id(url)
+        webpage = self._download_webpage(url, id)
         title = self._og_search_title(webpage)
         page_type = self._search_regex(r'https?://(?:www\.)?nhaccuatui\.com/([^\/]+)', url, 'page type', fatal=False)
         
@@ -40,29 +134,21 @@ class NhacCuaTuiIE(InfoExtractor):
             r'player.peConfig.xmlURL\s*=\s*"([^"]+)',
         ], webpage, 'player xml url')
 
-        player_xml = self._download_xml(player_xml_url, video_id, 'Downloading file XML')
-        item_element = player_xml.findall('./track')
+        player_xml = self._download_xml(player_xml_url, id, 'Downloading file XML')
+        items = player_xml.findall('./track')
 
-        formats = []
+        entries = []
 
-        for item in item_element:
-            creator = item.find('creator').text.strip()
-            location = item.find('location').text.strip()
-            f = {
-                'format_id': '128',
-                'url': location,
-            }
+        for i, item in enumerate(items, 1):
+            entry = self._extract_item(item, page_type)
+            if page_type == 'playlist':
+                entries.append(entry)
+            else:
+                return entry
 
-            f.update({
-                'abr': 128,
-                'ext': 'mp3',
-            })
-            formats.append(f)
-            
         return {
-            'id': video_id,
+            '_type': 'playlist',
+            'id': id,
             'title': title,
-            'description': self._og_search_description(webpage),
-            'formats': formats,
-            'thumbnail': self._og_search_thumbnail(webpage),
+            'entries': entries,
         }
